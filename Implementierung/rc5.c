25776839f4cd003637536e5de9a1ae857c152b4c
Mete Polat <polatm@in.tum.de>
2019-07-11 16:11:59 +0200

    Rahmenprogramm

--- Implementierung/rc5.c
+++ Implementierung/rc5.c
@@ -1,114 +1,287 @@
+#include <bsd/stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <sysexits.h>
+#include <err.h>
+#include "rc5.h"
+
 #define ROUNDS 16
 #define BLOCKSIZE 4
 #define HALFBLOCK BLOCKSIZE/2
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <string.h>
+void *data = NULL;
 
-void pkcs7_pad(void *buf, size_t len);
-
-void rc5_cbc_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
-
-int rc5_cbc_dec(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
-
-extern void rc5_init(unsigned char *key, size_t keylen, void *s, void *l);
-
-extern void rc5_enc(uint16_t *buffer, void *s);
-
-extern void rc5_dec(uint16_t *buffer, void *s);
-
-int main() {
-    char block[] = "ERAP";
-    uint16_t* keys = malloc(34 * 2);
-    if(keys == NULL) return;
-    int i = 0;
-    for(int i = 0; i < 34; ++i) {
-        *(keys + i) = i + 1;
+void cleanup(void) {
+    if (data != NULL) {
+        free(data);
     }
-    printf("Plaintext: %s\n", block);
-    rc5_enc(block, keys);
-    printf("Verschlüsselt: %s\n", block);
-    rc5_dec(block, keys);
-    printf("Entschlüsselt: %s\n", block);
-    free(keys);
-    return 0;
+}
+
+void usage(const char *restrict program_name) {
+    errx(EX_USAGE,
+         "usage: %s (-d | --decrypt | -e | --encrypt) [--ctr] <key> <inputFile> [outputFile]",
+         program_name);
+}
+
+int main(int argc, char **argv) {
+    atexit(&cleanup);
+    char *program_name = argv[0];
+
+    int encrypt = 0, decrypt = 0, ctr = 0;
+
+    const struct option longopts[] = {
+            {"encrypt", no_argument, NULL, 'e'}, // entweder --encrypt oder -e
+            {"decrypt", no_argument, NULL, 'd'}, // entweder --decrypt oder -d
+            {"ctr",     no_argument, &ctr, 1}, // nur --ctr
+            {NULL,      no_argument, NULL, 0}
+            // Aus der man page von getopt_long:
+            // "The last element of the longopts array has to be filled with zeroes"
+            // Ansonsten kann es bei nicht bekanntem Argument zu einem segmentation fault kommen
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "ed", longopts, NULL)) != -1) {
+        switch (opt) {
+            case 'e':
+                encrypt = 1;
+                break;
+            case 'd':
+                decrypt = 1;
+                break;
+            case 0:
+                // Long options wie --ctr. getopt_long() setzt den im option struct übergebenen
+                // flag pointer auf das definierte val.
+                // Beispiel: {"ctr", no_argument, &ctr, 1}
+                // Wenn --ctr, dann wird die Variable ctr auf 1 gesetzt
+                break;
+            default:
+                usage(program_name);
+        }
+    }
+
+    // Entweder entschlüsseln oder verschlüsseln
+    if (!(encrypt ^ decrypt)) {
+        usage(program_name);
+    }
+
+    // optind entält den Index auf das nächste argv Argument
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 2) {
+        // Keine input Datei oder Schlüssel
+        usage(program_name);
+    }
+
+    const char *restrict key = argv[0];
+    const char *restrict inputFile = argv[1];
+    const char *restrict outputFile = argc == 3 ? argv[2] : argv[1];
+
+    size_t size = read_file(inputFile, NULL, 0);
+    if (size == -1) {
+        // Fehler beim öffnen oder bestimmen der Dateigröße
+        err(EX_IOERR, "%s", inputFile);
+    }
+
+    data = malloc(size);
+    if (!data) {
+        err(EX_OSERR, NULL);
+    }
+
+    if (read_file(inputFile, data, size)) {
+        err(EX_IOERR, "%s", inputFile);
+    }
+
+    if (ctr) {
+        warnx("%s", "Counter Mode (CTR) not yet supported. Using Cipher Block Chaining (CBC)");
+    }
+
+    if (encrypt) {
+        // data enthält nur den zu verschlüsselnden Dateiinhalt
+
+        uint32_t iv = arc4random(); // init vector
+        size_t size_pad = BLOCKSIZE - (size % BLOCKSIZE);
+        size_t size_all = size + size_pad + sizeof(iv);
+        // Erweitere Speicher um padding und iv zu speichern
+        data = reallocf(data, size_all);
+        if (data == NULL) {
+            err(EX_OSERR, NULL);
+        }
+
+        // Füge padding nach Dateiinhalt an
+        pkcs7_pad((uint8_t *) data + size, size_pad);
+
+        // Verschlüssel Dateiinhalt + padding
+        rc5_cbc_enc((unsigned char *) key, strlen(key), data, size + size_pad, iv);
+
+        // Füge init vector ans Dateiende (nach padding)
+        ((uint32_t *) data)[size + size_pad / sizeof(uint32_t)] = iv;
+        size = size_all;
+    } else {
+        // data enthält den zu entschlüsselnden Dateiinhalt und padding + init vector am Ende der
+        // Datei (unverschlüsselt)
+
+        // init vector am Dateiende
+        uint32_t iv = ((uint32_t *) data)[size / sizeof(uint32_t) - 1];
+        size = rc5_cbc_dec((unsigned char *) key, strlen(key), data, size - sizeof(iv), iv);
+        if (size == -1) {
+            errx(EX_DATAERR, "%s: Could not decrypt. File is malformed", inputFile);
+        }
+    }
+
+    if (write_file(outputFile, data, size)) {
+        err(EX_IOERR, "%s", outputFile);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+/**
+ * Liest die Datei in path in buffer. Falls buffer NULL ist, gibt die Funktion die Dateigröße
+ * zurück.
+ * @param path Der Pfad zur Datei, die gelesen werden soll.
+ * @param buffer Der Buffer in dem die Datei gespeichert werden soll. Wenn NULL, gibt die Funktion
+ * die Dateigröße in Bytes zurück.
+ * @param size Die Größe des Buffers.
+ * @return Gibt die Dateigröße in Bytes zurück wenn buffer NULL ist, ansonsten wird 0
+ * zurückgegeben. Falls ein Fehler auftritt, wird -1 zurückgegeben.
+ */
+long read_file(const char *restrict path, void *restrict buffer, size_t size) {
+    // Öffne Datei, r = read mode
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        return -1;
+    }
+
+    if (buffer == NULL) {
+        // Springe zum Dateiende um Dateigröße herauszufinden
+        if (fseek(file, 0, SEEK_END)) {
+            // Schließe Datei und behalte ursprünglichen errno
+            fclose_keep_errno(file);
+            return -1;
+        }
+
+        // "File position indicator" am Ende der Datei, ftell() gibt also Dateigröße in Bytes zurück
+        size = ftell(file);
+        if (size == -1) {
+            fclose_keep_errno(file);
+            return -1;
+        }
+
+        if (fclose(file)) {
+            return -1;
+        }
+
+        return size;
+    }
+
+    // Lies kompletten Dateiinhalt in den allokierten Speicher
+    if (fread(buffer, 1, size, file) != size) {
+        fclose(file);
+        // Setze errno manuell, da fread() errno nicht setzt
+        errno = EIO;
+        return -1;
+    }
+
+    return fclose(file);
+}
+
+/**
+ * Speichert buffer an den angegeben Pfad.
+ * @param path Der Pfad an den buffer gespeichert werden soll.
+ * @param buffer Der Buffer, der die zu speichernden Daten enthält.
+ * @param size Die Größe des Buffers
+ * @return Gibt 0 zuück wenn erfolgreich, ansonsten -1.
+ *
+ */
+int write_file(const char *restrict path, const void *restrict buffer, size_t size) {
+    // Öffne Datei, w = write mode
+    FILE *file = fopen(path, "w");
+    if (file == NULL) {
+        return -1;
+    }
+
+    // Schreibe den kompletten buffer in die Datei
+    if (fwrite(buffer, 1, size, file) != size) {
+        fclose(file);
+        errno = EIO;
+        return -1;
+    }
+
+    return fclose(file);
+}
+
+/**
+ * Helfermethode, die eine Datei schließt und dabei den Wert von errno beibehält. Hilfreich falls
+ * fclose(file) aufgrund eines vorangeganenen Fehlers aufgerufen werden soll, aber fclose nicht
+ * den Fehlercode überschreiben soll, falls auch in dieser es zu einem Fehler kommt.
+ *
+ * @param file Die zu schließende Datei.
+ */
+void fclose_keep_errno(FILE *file) {
+    int tmp = errno;
+    fclose(file);
+    errno = tmp;
 }
 
 void pkcs7_pad(void *buf, size_t len) {
-
+    for (int i = 0; i < len; i++) {
+        ((uint8_t *) buf)[i] = len;
+    }
 }
 
 void rc5_cbc_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv) {
-    void *roundkeys;
-    void *l;
-    uint32_t *padbuf;
-    uint32_t *curblock, *lastblock;
-    size_t block_count, i;
     // allokiere Speicherbereich für die Rundenschluessel
     // 2r+2 Schluessel zu je 16 Bit laenge
-    roundkeys = malloc((2 * ROUNDS + 2) * HALFBLOCK);
-    l = malloc((2 * ROUNDS + 2) * HALFBLOCK);
+    void *roundkeys = malloc((2 * ROUNDS + 2) * HALFBLOCK);
+    void *l = malloc((2 * ROUNDS + 2) * HALFBLOCK);
 
     // Keysetup
     rc5_init(key, keylen, roundkeys, l);
 
-    // allokiere Platz fuer das padding
-    padbuf = (uint32_t *) malloc(len + (len % BLOCKSIZE));
-    memcpy(padbuf, buffer, len);
-    block_count = sizeof(padbuf) / BLOCKSIZE;
+    // benutze iv um den ersten block zu XORen
+    *buffer ^= iv;
+    rc5_enc(buffer, roundkeys);
 
-    pkcs7_pad((padbuf + (block_count - 1)), BLOCKSIZE - (len % BLOCKSIZE));
-
-
-    // benutze einen initialization vector
-    // um den ersten block zu XORen
-    *padbuf ^= iv;
-    rc5_enc((uint16_t *) padbuf, roundkeys);
-    curblock = padbuf;
-    for (i = 1; i < block_count; i++) {
-        lastblock = curblock++;
-        *curblock ^= *lastblock;
-        rc5_enc(curblock, roundkeys);
+    for (size_t i = 1; i < len / BLOCKSIZE; i++) {
+        uint32_t *lastblock = buffer++;
+        *buffer ^= *lastblock;
+        rc5_enc(buffer, roundkeys);
     }
+
     free(roundkeys);
     free(l);
 }
 
 int rc5_cbc_dec(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv) {
-    void *roundkeys;
-    void *l;
-    uint32_t *padbuf;
-    uint32_t *curblock, *lastblock;
-    uint32_t lastenc, curenc;
-    size_t block_count, i;
     // allokiere Speicherbereich für die Rundenschluessel
     // 2r+2 Schluessel zu je 16 Bit laenge
-    roundkeys = malloc((2 * ROUNDS + 2) * HALFBLOCK);
-    l = malloc((2 * ROUNDS + 2) * HALFBLOCK);
+    void *roundkeys = malloc((2 * ROUNDS + 2) * HALFBLOCK);
+    void *l = malloc((2 * ROUNDS + 2) * HALFBLOCK);
 
     // Keysetup
     rc5_init(key, keylen, roundkeys, l);
 
-    if (len % BLOCKSIZE != 0)
-        return -1; // error
+    if (len % BLOCKSIZE != 0) {
+        return -1;
+    }
 
-    block_count = len / BLOCKSIZE;
-    lastenc = *buffer;
-    rc5_dec((uint16_t *) buffer, roundkeys);
+    uint32_t lastenc = *buffer;
+    // benutze iv um den ersten block zu XORen
+    rc5_dec(buffer, roundkeys);
     *buffer ^= iv;
 
-    // benutze einen initialization vector
-    // um den ersten block zu XORen
-    for (i = 1; i < block_count; i++) {
-        curenc = *++buffer;
+    for (size_t i = 1; i < len / BLOCKSIZE; i++) {
+        uint32_t curenc = *(++buffer);
         rc5_dec(buffer, roundkeys);
         *buffer ^= lastenc;
         lastenc = curenc;
     }
+
     free(roundkeys);
-    // gibt laenge ohne padding zurueck
-    return len - (*buffer & 0xFF);
+    free(l);
+
+    // gibt size ohne padding zurück
+    return len - (*buffer & 0xFFu);
 }
---
+++ Implementierung/rc5.h
@@ -0,0 +1,26 @@
+#ifndef RC5_RC5_H
+#define RC5_RC5_H
+
+void cleanup(void);
+
+long read_file(const char *restrict path, void *restrict buffer, size_t size);
+
+int write_file(const char *restrict path, const void *restrict data, size_t size);
+
+void fclose_keep_errno(FILE *file);
+
+void pkcs7_pad(void *buf, size_t len);
+
+void rc5_cbc_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
+
+int rc5_cbc_dec(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
+
+void usage(const char *restrict program_name);
+
+extern void rc5_init(unsigned char *key, size_t keylen, void *s, void *l);
+
+extern void rc5_enc(uint32_t *buffer, void *s);
+
+extern void rc5_dec(uint32_t *buffer, void *s);
+
+#endif
