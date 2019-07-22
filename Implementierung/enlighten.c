769a2e188de3f04afa59fbf0f3e30ba84cde847a
Mete Polat <polatm@in.tum.de>
2019-07-22 14:33:37 +0200

    Fix padding Funktion
    Die Schleifenbedingung war nicht ganz richtig weshalb das padding nicht
    funktioniert hat.
    Außerdem war die Klammerung beim cast nicht ganz richtig. gcc erlaubt
    zwar arithmetische Operationen auf void Pointern (behandelt diese dann
    wie ein char Pointer) aber das entspricht nicht dem C Standard. Die
    Option -pedantic gibt Warnungen aus falls etwas nicht dem Standard
    entspricht.
    
    Siehe:
    https://stackoverflow.com/a/6449948
    https://stackoverflow.com/q/2855121

---
+++ Implementierung/enlighten.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "bufferio.h"
+
+#define BMP_HEADER_LENGTH 54
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("usage: %s <originalBMP> <encryptedBMP>\n", argv[0]);
+        return 1;
+    }
+
+    char *pathOrigin = argv[1];
+    char *pathEnc = argv[2];
+
+    size_t size_original = read_file(pathOrigin, NULL, 0);
+    size_t size_enc = read_file(pathEnc, NULL, 0);
+    if (size_original == -1u) {
+        perror(pathOrigin);
+        return -1;
+    }
+
+    if (size_enc == -1u) {
+        perror(pathEnc);
+        return -1;
+    }
+
+    if (size_enc <= size_original) {
+        printf("One of the given files is malformed!");
+        return -1;
+    }
+
+    void *origin = malloc(BMP_HEADER_LENGTH);
+    void *enc = malloc(size_original);
+
+    if (((uint32_t *) origin)[14] != 40 && // Nur Version 3
+        ((uint16_t *) origin)[28] < 24 &&  // Keine Indizierung
+        ((uint32_t *) origin)[30] != 0) { // keine Kompression
+        printf("Es wird nur BMP 3 ohne Indizierung und Kompression unterstützt\n");
+    }
+
+    if (read_file(pathOrigin, origin, BMP_HEADER_LENGTH)) {
+        perror(pathOrigin);
+        free(origin);
+        free(enc);
+        return -1;
+    }
+
+    if (read_file(pathEnc, enc, size_original)) {
+        perror(pathOrigin);
+        free(origin);
+        free(enc);
+        return -1;
+    }
+
+    memcpy(enc, origin, BMP_HEADER_LENGTH);
+    if (write_file(pathEnc, enc, size_original)) {
+        printf("Could not write enc\n");
+    }
+
+    free(origin);
+    free(enc);
+    return 0;
+}
--- Implementierung/rc5.c
+++ Implementierung/rc5.c
@@ -1,9 +1,9 @@
 #include <string.h>
-#include <errno.h>
 #include <getopt.h>
 #include <sysexits.h>
 #include <err.h>
 #include "rc5.h"
+#include "bufferio.h"
 
 #define ROUNDS 16
 #define BLOCKSIZE 4
@@ -165,94 +165,6 @@ int main(int argc, char **argv) {
     }
 
     exit(EXIT_SUCCESS);
-}
-
-/**
- * Liest die Datei in path in buffer. Falls buffer NULL ist, gibt die Funktion die Dateigröße
- * zurück.
- * @param path Der Pfad zur Datei, die gelesen werden soll.
- * @param buffer Der Buffer in dem die Datei gespeichert werden soll. Wenn NULL, gibt die Funktion
- * die Dateigröße in Bytes zurück.
- * @param size Die Größe des Buffers.
- * @return Gibt die Dateigröße in Bytes zurück wenn buffer NULL ist, ansonsten wird 0
- * zurückgegeben. Falls ein Fehler auftritt, wird -1 zurückgegeben.
- */
-long read_file(const char *restrict path, void *restrict buffer, size_t size) {
-    // Öffne Datei, r = read mode
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        return -1;
-    }
-
-    if (buffer == NULL) {
-        // Springe zum Dateiende, um Dateigröße herauszufinden
-        if (fseek(file, 0, SEEK_END)) {
-            // Schließe Datei und behalte ursprünglichen errno
-            fclose_keep_errno(file);
-            return -1;
-        }
-
-        // "File position indicator" am Ende der Datei, ftell() gibt also Dateigröße in Bytes zurück
-        size = ftell(file);
-        if (size == -1) {
-            fclose_keep_errno(file);
-            return -1;
-        }
-
-        if (fclose(file)) {
-            return -1;
-        }
-
-        return size;
-    }
-
-    // Lies kompletten Dateiinhalt in den allokierten Speicher
-    if (fread(buffer, 1, size, file) != size) {
-        fclose(file);
-        // Setze errno manuell, da fread() errno nicht setzt
-        errno = EIO;
-        return -1;
-    }
-
-    return fclose(file);
-}
-
-/**
- * Speichert buffer an den angegeben Pfad.
- * @param path Der Pfad an den buffer gespeichert werden soll.
- * @param buffer Der Buffer, der die zu speichernden Daten enthält.
- * @param size Die Größe des Buffers
- * @return Gibt 0 zuück wenn erfolgreich, ansonsten -1.
- *
- */
-int write_file(const char *restrict path, const void *restrict buffer, size_t size) {
-    // Öffne Datei, w = write mode
-    FILE *file = fopen(path, "w");
-    if (file == NULL) {
-        return -1;
-    }
-
-    // Schreibe den kompletten buffer in die Datei
-    if (fwrite(buffer, 1, size, file) != size) {
-        fclose(file);
-        errno = EIO;
-        return -1;
-    }
-
-    return fclose(file);
-}
-
-/**
- * Helfermethode, die eine Datei schließt und dabei den Wert von errno beibehält. Hilfreich falls
- * fclose(file) aufgrund eines vorangeganenen Fehlers aufgerufen werden soll, aber fclose nicht
- * den Fehlercode überschreiben soll, falls es auch in dieser zu einem Fehler kommt.
- *
- * @param file Die zu schließende Datei.
- */
-void fclose_keep_errno(FILE *file) {
-    int tmp = errno;
-    fclose(file);
-    errno = tmp;
 }
 
 void pkcs7_pad(void *buf, size_t len) {
