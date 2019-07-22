b6b0282c8085726b1942bef2f5415cf515165f3b
Jonas Hübotter <jonas.huebotter@tum.de>
2019-07-22 13:24:42 +0200

    Fix padding Funktion
    Die Schleifenbedingung war nicht ganz richtig weshalb das padding nicht
    funktioniert hat.
    Außerdem war die Klammerung beim cast nicht ganz richtig. gcc erlaubt
    zwar arithmetische Operationen auf void Pointern (behandelt diese dann
    wie ein char Pointer) aber das entspricht nicht dem C Standard. Die
    Option -pedantic gibt Warnungen aus falls etwas nicht dem Standard
    entspricht.
    
    Siehe:
    https://stackoverflow.com/a/6449948
    https://stackoverflow.com/q/2855121

--- Implementierung/rc5.h
+++ Implementierung/rc5.h
@@ -17,6 +17,12 @@ void rc5_cbc_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len
 
 void rc5_cbc_dec(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
 
+void rc5_ctr(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len);
+
+void rc5_ecb_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len);
+
+void rc5_ecb_dec(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len);
+
 void usage(const char *restrict program_name);
 
 extern void rc5_init(unsigned char *key, size_t keylen, void *l);
@@ -24,5 +30,9 @@ extern void rc5_init(unsigned char *key, size_t keylen, void *l);
 extern void rc5_enc(uint32_t *buffer);
 
 extern void rc5_dec(uint32_t *buffer);
+
+extern void rc5_enc_128(uint32_t *buffer);
+
+extern void reset_registers();
 
 #endif
