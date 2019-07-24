48a49547ddd07316dae10eb11c1d673f31f3bfcb
Mete Polat <polatm@in.tum.de>
2019-07-24 13:27:51 +0200

    Performance Testing

---
+++ Implementierung/perf.h
@@ -0,0 +1,6 @@
+#ifndef RC5_PERF_H
+#define RC5_PERF_H
+
+void run_perf_tests();
+
+#endif
--- Implementierung/rc5.h
+++ Implementierung/rc5.h
@@ -7,10 +7,6 @@
 #define BLOCKSIZE 4
 #define HALFBLOCK BLOCKSIZE/2
 
-#define ROUNDS 16
-#define BLOCKSIZE 4
-#define HALFBLOCK BLOCKSIZE/2
-
 void pkcs7_pad(void *buf, size_t len);
 
 void rc5_cbc_enc(unsigned char *key, size_t keylen, uint32_t *buffer, size_t len, uint32_t iv);
