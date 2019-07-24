caaf868c91deefd9c40ad631ce2b5073753504b0
Simon Martin Bohnen <bohnen@in.tum.de>
2019-07-24 13:30:20 +0200

    Fix typo in array length

--- Implementierung/test.h
+++ Implementierung/test.h
@@ -11,6 +11,6 @@ void run_test(char *id);
 test_vector *setup_RFC2040_testvector(int padding, char *key, size_t key_length, char *plain,
                                       int plain_length, uint32_t iv);
 
-extern uint16_t roundkeys[34./];
+extern uint16_t roundkeys[34];
 
 #endif
